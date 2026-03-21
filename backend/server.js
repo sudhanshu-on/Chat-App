c400449f2ae0443773692b781316a59ffb952d47
Sudhanshu Bagde <sudhanshubagde1234@gmail.com>
2026-03-21 18:53:49 +0530

    fixed localhost

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,7 @@ app.use("/api/conversations", conversationRoutes);
 
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT 
 
 server.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
